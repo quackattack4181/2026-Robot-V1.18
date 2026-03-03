c7a08cdd7a35a63c69a2993cccfa1861dbbafca2
QuackAttack-Team4181 <162037610+quackattack4181@users.noreply.github.com>
2026-03-03 13:24:11 -0600

    Update intake pivot offset and tolerance to provided values

--- java/frc/robot/Constants.java
+++ java/frc/robot/Constants.java
@@ -196,14 +196,14 @@ public final class Constants
     public static final double PIVOT_POWER = 0.30;
     public static final boolean PIVOT_LIMITS_ENABLED = true;
     // Absolute encoder offset in degrees. Set this so your desired "zero" angle reads 0.0.
-    public static final double PIVOT_ABSOLUTE_ENCODER_OFFSET_DEGREES = 0.0;
+    public static final double PIVOT_ABSOLUTE_ENCODER_OFFSET_DEGREES = -141.0;
     // If true, flip encoder sign so angle increases/decreases opposite direction.
     public static final boolean PIVOT_ENCODER_DIRECTION_INVERTED = false;
     // Manual soft limits (degrees) after applying offset.
     // Typical setup requested: inward = 0.0, outward = positive value.
     public static final double PIVOT_MAX_INWARD_ANGLE = 0.0;
     public static final double PIVOT_MAX_OUTWARD_ANGLE = 90.0;
-    public static final double PIVOT_ANGLE_TOLERANCE_DEGREES = 2.0;
+    public static final double PIVOT_ANGLE_TOLERANCE_DEGREES = 0.5;
     public static final double WHEEL_POWER = 0.80;
   }
 }
